f29be13ba40481a2f6a09583c05839a3bb22ba8e
app-app-app-app <lobanskijila@gmail.com>
2026-03-09 10:53:14 +0200

    Update index.php

--- templates/template_2/index.php
+++ templates/template_2/index.php
@@ -83,10 +83,10 @@ include 'lang.php';
 
   <meta name="description" content="<?= $main_description ?>">
   <title><?= $main_title ?></title>
-  <link rel="icon" type="image/png" href="favicon-96x96.png" sizes="96x96" />
-  <link rel="icon" type="image/svg+xml" href="favicon.svg" />
-  <link rel="shortcut icon" href="favicon.ico" />
-  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+  <link rel="icon" type="image/png" href="./favicon-96x96.png" sizes="96x96" />
+  <link rel="icon" type="image/svg+xml" href="./favicon.svg" />
+  <link rel="shortcut icon" href="./favicon.ico" />
+  <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
   <link rel="manifest" href="site.webmanifest" />
   <link rel="stylesheet" href="fonts.css">
   <link rel="stylesheet" href="tailwind.css">
