ccec895d52fed3fdca5daa1688d3f735507312d3
app-app-app-app <lobanskijila@gmail.com>
2026-03-24 15:04:26 +0200

    Update about.php

--- templates/template_1-1/about.php
+++ templates/template_1-1/about.php
@@ -3,7 +3,13 @@ session_start();
 require_once 'offer_seo.php';
 include 'lang.php';
 ?>
-
+<?php
+if (strpos($_SERVER['HTTP_HOST'], 'www.') === 0) {
+    $host = substr($_SERVER['HTTP_HOST'], 4);
+    header("Location: https://" . $host . $_SERVER['REQUEST_URI'], true, 301);
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html
   lang="<?= $site_lang ?>"
